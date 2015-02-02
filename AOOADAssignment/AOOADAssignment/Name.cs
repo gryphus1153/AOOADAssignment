6348ec0f472d1fd3ba626a126774f20239439ba3
gryphus1153 <gryphus1153@hotmail.com>
2015-02-02 17:30:18 +0800

    fsggdgsds

---
+++ AOOADAssignment/AOOADAssignment/Name.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOOADAssignment
+{
+    class Name
+    {
+        public Name(string fName, string mName, string lName)
+        {
+            FirstName = fName;
+            MiddleName = mName;
+            LastName = lName;
+        }
+
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+}
---
+++ AOOADAssignment/AOOADAssignment/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOOADAssignment
+{
+    class User
+    {
+        public User() { }
+        public User(string uID, string pass, Name name, string email, Address address) 
+        {
+            UserID = uID;
+            Password = pass;
+            Name = name;
+            Email = email;
+            Address = address;
+        }
+
+        public string UserID { get; set; }
+        public string Password { get; set; }
+        public Name Name { get; set; }
+        public string Email { get; set; }
+        public Address Address { get; set; }
+    }
+}
