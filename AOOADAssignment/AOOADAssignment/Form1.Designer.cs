e54c7cea5bea7eb0b447c7696df5713e4bf88eac
popo <stormy2nd0@gmail.com>
2015-02-03 21:19:19 +0800

    ListViewChanges

--- AOOADAssignment/AOOADAssignment/Form1.Designer.cs
+++ AOOADAssignment/AOOADAssignment/Form1.Designer.cs
@@ -34,17 +34,22 @@
             this.passwordTextBox = new System.Windows.Forms.TextBox();
             this.btnLogin = new System.Windows.Forms.Button();
             this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.confirmEventTxtBox = new System.Windows.Forms.TextBox();
+            this.createEventTxtBox = new System.Windows.Forms.TextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.browseEventDisplay = new System.Windows.Forms.TextBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.EventIDToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.NameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.StartDateToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.label3 = new System.Windows.Forms.Label();
+            this.browseListView = new System.Windows.Forms.ListView();
+            this.eventNameCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.startDateCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.endDateCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.followHeader = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage3.SuspendLayout();
             this.tabPage1.SuspendLayout();
-            this.menuStrip1.SuspendLayout();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -94,6 +99,10 @@
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.label5);
+            this.tabPage3.Controls.Add(this.label4);
+            this.tabPage3.Controls.Add(this.confirmEventTxtBox);
+            this.tabPage3.Controls.Add(this.createEventTxtBox);
             this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
             this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
@@ -101,6 +110,40 @@
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Create/Close/Confirm Event";
             this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(17, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Confirm Event";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Create Event";
+            // 
+            // confirmEventTxtBox
+            // 
+            this.confirmEventTxtBox.Location = new System.Drawing.Point(20, 174);
+            this.confirmEventTxtBox.Multiline = true;
+            this.confirmEventTxtBox.Name = "confirmEventTxtBox";
+            this.confirmEventTxtBox.Size = new System.Drawing.Size(722, 124);
+            this.confirmEventTxtBox.TabIndex = 1;
+            // 
+            // createEventTxtBox
+            // 
+            this.createEventTxtBox.Location = new System.Drawing.Point(20, 28);
+            this.createEventTxtBox.Multiline = true;
+            this.createEventTxtBox.Name = "createEventTxtBox";
+            this.createEventTxtBox.Size = new System.Drawing.Size(722, 127);
+            this.createEventTxtBox.TabIndex = 0;
             // 
             // tabPage2
             // 
@@ -114,8 +157,7 @@
             // 
             // tabPage1
             // 
-            this.tabPage1.Controls.Add(this.browseEventDisplay);
-            this.tabPage1.Controls.Add(this.menuStrip1);
+            this.tabPage1.Controls.Add(this.browseListView);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
@@ -124,48 +166,6 @@
             this.tabPage1.Text = "Browse Event";
             this.tabPage1.UseVisualStyleBackColor = true;
             this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
-            // 
-            // browseEventDisplay
-            // 
-            this.browseEventDisplay.Location = new System.Drawing.Point(6, 63);
-            this.browseEventDisplay.Multiline = true;
-            this.browseEventDisplay.Name = "browseEventDisplay";
-            this.browseEventDisplay.Size = new System.Drawing.Size(750, 248);
-            this.browseEventDisplay.TabIndex = 0;
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.EventIDToolStripMenuItem,
-            this.NameToolStripMenuItem,
-            this.StartDateToolStripMenuItem1});
-            this.menuStrip1.Location = new System.Drawing.Point(3, 3);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(756, 24);
-            this.menuStrip1.TabIndex = 1;
-            this.menuStrip1.Text = "menuStrip1";
-            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
-            // 
-            // EventIDToolStripMenuItem
-            // 
-            this.EventIDToolStripMenuItem.Name = "EventIDToolStripMenuItem";
-            this.EventIDToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
-            this.EventIDToolStripMenuItem.Text = "Event ID";
-            this.EventIDToolStripMenuItem.Click += new System.EventHandler(this.EventIDToolStripMenuItem_Click);
-            // 
-            // NameToolStripMenuItem
-            // 
-            this.NameToolStripMenuItem.Name = "NameToolStripMenuItem";
-            this.NameToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
-            this.NameToolStripMenuItem.Text = "Name";
-            this.NameToolStripMenuItem.Click += new System.EventHandler(this.NameToolStripMenuItem_Click);
-            // 
-            // StartDateToolStripMenuItem1
-            // 
-            this.StartDateToolStripMenuItem1.Name = "StartDateToolStripMenuItem1";
-            this.StartDateToolStripMenuItem1.Size = new System.Drawing.Size(70, 20);
-            this.StartDateToolStripMenuItem1.Text = "Start Date";
-            this.StartDateToolStripMenuItem1.Click += new System.EventHandler(this.startDateToolStripMenuItem_Click);
             // 
             // tabControl1
             // 
@@ -187,6 +187,44 @@
             this.label3.Size = new System.Drawing.Size(0, 13);
             this.label3.TabIndex = 6;
             // 
+            // browseListView
+            // 
+            this.browseListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.eventNameCol,
+            this.startDateCol,
+            this.endDateCol,
+            this.buttonHeader,
+            this.followHeader});
+            this.browseListView.FullRowSelect = true;
+            this.browseListView.Location = new System.Drawing.Point(18, 36);
+            this.browseListView.Name = "browseListView";
+            this.browseListView.Size = new System.Drawing.Size(722, 263);
+            this.browseListView.TabIndex = 3;
+            this.browseListView.UseCompatibleStateImageBehavior = false;
+            // 
+            // eventNameCol
+            // 
+            this.eventNameCol.Text = "Event Name";
+            this.eventNameCol.Width = 140;
+            // 
+            // startDateCol
+            // 
+            this.startDateCol.Text = "Start Date";
+            this.startDateCol.Width = 140;
+            // 
+            // endDateCol
+            // 
+            this.endDateCol.Text = "End Date";
+            this.endDateCol.Width = 140;
+            // 
+            // buttonHeader
+            // 
+            this.buttonHeader.Text = "Buy Ticket";
+            // 
+            // followHeader
+            // 
+            this.followHeader.Text = "Follow";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -200,13 +238,11 @@
             this.Controls.Add(this.loginIDTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tabControl1);
-            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Brotic System";
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
             this.tabPage1.ResumeLayout(false);
-            this.tabPage1.PerformLayout();
-            this.menuStrip1.ResumeLayout(false);
-            this.menuStrip1.PerformLayout();
             this.tabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -223,13 +259,18 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TextBox browseEventDisplay;
-        private System.Windows.Forms.MenuStrip menuStrip1;
-        private System.Windows.Forms.ToolStripMenuItem EventIDToolStripMenuItem;
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.ToolStripMenuItem NameToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem StartDateToolStripMenuItem1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox confirmEventTxtBox;
+        private System.Windows.Forms.TextBox createEventTxtBox;
+        private System.Windows.Forms.ListView browseListView;
+        private System.Windows.Forms.ColumnHeader eventNameCol;
+        private System.Windows.Forms.ColumnHeader startDateCol;
+        private System.Windows.Forms.ColumnHeader endDateCol;
+        private System.Windows.Forms.ColumnHeader buttonHeader;
+        private System.Windows.Forms.ColumnHeader followHeader;
     }
 }
 
--- AOOADAssignment/AOOADAssignment/Form1.cs
+++ AOOADAssignment/AOOADAssignment/Form1.cs
@@ -11,9 +11,9 @@ using System.Collections;
 namespace AOOADAssignment
 {
     public partial class Form1 : Form
-    { 
+    {
         static ArrayList userList = new ArrayList();
-            
+        static ArrayList EventList = new ArrayList();
         public Form1()
         {
             Address addr = new Address("Block 123", "Street1234", "uNo", "#01-900");
@@ -24,20 +24,35 @@ namespace AOOADAssignment
             User cust1 = new Customer("uID", "pass", name, "email@email.com", addr, "999666999", cc, tickList, payList);
             userList.Add(cust1);
 
+            DateTime time = Convert.ToDateTime("3/2/2015 12:00");
+            DateTime endTime = Convert.ToDateTime("3/2/2015 18:00");
+            Genre eg = new Genre("Fantasy");
+            ArrayList performancesList = new ArrayList();
+            ArrayList sManagers = new ArrayList();
+            EventManager el = new EventManager();
+            Event event1 = new Event("Disney Fat Princesses", time, endTime, eg, performancesList, 100, "Open", el, sManagers, true);
+            EventList.Add(event1);
+
+            DateTime time1 = Convert.ToDateTime("4/4/2015 12:00");
+            DateTime endTime1 = Convert.ToDateTime("4/4/2015 18:00");
+            Genre eg1 = new Genre("Dance");
+            ArrayList performancesList1 = new ArrayList();
+            ArrayList sManagers1 = new ArrayList();
+            EventManager el1 = new EventManager();
+            Event event2 = new Event("Dance dance?!", time1, endTime1, eg1, performancesList1, 100, "Open", el1, sManagers1, true);
+            EventList.Add(event2);
 
             InitializeComponent();
-        }
-
-        private void EventIDToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void startDateToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
         }
+        private void EventNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
 
+        }
         private void NameToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
@@ -46,25 +61,25 @@ namespace AOOADAssignment
         private void btnLogin_Click(object sender, EventArgs e)
         {
             bool exist = false;
-            
-           foreach(User i in userList)
-           {
-               if(i.UserID.Equals(loginIDTextBox.Text))
-               {
-                   string pass = i.Password;
-                   if(pass.Equals(passwordTextBox.Text))
-                   {
-                       tabControl1.Enabled = true;
-                       tabControl1.Visible = true;
-                       exist = true;
-                       break;
-                   }
-               }
-               tabControl1.Enabled = false;
-               tabControl1.Visible = false;
-               exist = false;
-           }
-            if(exist==true)
+
+            foreach (User i in userList)
+            {
+                if (i.UserID.Equals(loginIDTextBox.Text))
+                {
+                    string pass = i.Password;
+                    if (pass.Equals(passwordTextBox.Text))
+                    {
+                        tabControl1.Enabled = true;
+                        tabControl1.Visible = true;
+                        exist = true;
+                        break;
+                    }
+                }
+                tabControl1.Enabled = false;
+                tabControl1.Visible = false;
+                exist = false;
+            }
+            if (exist == true)
             {
                 //TextBox t = new TextBox();
                 //t.Text = "Logged in successfully";
@@ -75,23 +90,42 @@ namespace AOOADAssignment
                 label3.Text = "Log in unsuccessfully";
             }
         }
-        
-        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
-        {
 
-        }
 
         private void tabPage1_Click(object sender, EventArgs e)
         {
-            DateTime time = Convert.ToDateTime("3/2/2015 12:00");
-            DateTime endTime = Convert.ToDateTime("3/2/2015 18:00");
-            Genre eg = new Genre("Fantasy");
-            ArrayList performancesList = new ArrayList();
-            ArrayList sManagers = new ArrayList();
-            EventManager el = new EventManager();
-            Event event1 = new Event("Disney Fat Princesses", time, endTime, eg, performancesList, 100, "Open", el, sManagers, true);
-            browseEventDisplay.Text += event1.EventName + "\t" + event1.StartDate + "\t" + event1.EndDate;
+            browseListView.View = View.Details;
+            /*browseListView.Columns.Add("Event Name", -2, HorizontalAlignment.Center);
+            browseListView.Columns.Add("Start Date", -2, HorizontalAlignment.Center);
+            browseListView.Columns.Add("End Date", -2, HorizontalAlignment.Center);*/
 
+           // browseListView.Columns.Add("Event Name");
+           // browseListView.Columns.Add("Start Date");
+           // browseListView.Columns.Add("End Date");
+
+           // browseListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+           // browseListView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
+
+            ListViewExtender extender = new ListViewExtender(browseListView);
+            // extend 2nd column
+            ListViewButtonColumn buttonAction = new ListViewButtonColumn(3);
+            //buttonAction.Click += OnButtonActionClick;
+            buttonAction.FixedWidth = true;
+            ListViewButtonColumn buttonAction2 = new ListViewButtonColumn(4);
+            buttonAction2.FixedWidth = true;
+            extender.AddColumn(buttonAction);
+            extender.AddColumn(buttonAction2);
+            foreach (Event evt in EventList)
+            {
+                if (evt.Status.Equals("Open"))
+                {
+                    //browseEventDisplay.Text += evt.EventName + "\t" + evt.StartDate + "\t" + evt.EndDate;
+                    ListViewItem item = new ListViewItem(new[] { evt.EventName.ToString(), evt.StartDate.ToString(), evt.EndDate.ToString(), "Buy" , "Follow"});
+                    browseListView.Items.Add(item);
+                }
+            }
         }
+
+
     }
 }
