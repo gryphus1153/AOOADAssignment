f4439ba24d4b9d0cba051ff55fe2ba28e2654120
Gryphus1153 <gryphus1153@hotmail.com>
2015-02-06 15:55:54 +0800

    Add Performane and stuff

---
+++ AOOADAssignment/AOOADAssignment/AddPerformance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace AOOADAssignment
+{
+    public partial class AddPerformance : Form
+    {
+        
+        public AddPerformance()
+        {
+            InitializeComponent();
+        }
+
+        public Performance getPerformance()
+        {
+            Performance p;
+            ArrayList perfomersList = new ArrayList();
+            foreach (string i in PerformersListBox.CheckedItems)
+	        {
+                perfomersList.Add(i);
+	        }
+            p = new Performance(Convert.ToString(StartdateTimePicker1), Convert.ToString(EnddateTimePicker), perfomersList);
+            return p;
+
+        }
+    }
+}
